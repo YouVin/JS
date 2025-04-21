c7f63d6a57ca7748c40529588565e20a674bcdd5
YouVin <dbqls990909@naver.com>
2025-04-21 16:45:28 +0900

    기본값 매개변수

--- workspace/ch01/ex01-42.js
+++ workspace/ch01/ex01-42.js
@@ -2,5 +2,17 @@
 기본값 매개변수(Default parameters)
 */
 
+function sum(a, b) {
+  if (b === undefined) {
+    b = 0;
+  }
+  return a + b;
+}
 
+function sum(a, b) {
+  b = b || 0;
+  return a + b;
+}
 
+console.log(sum(10, 20));
+console.log(sum(50));
