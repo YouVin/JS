8255c80d4638c8ae7769950143eb1461e46f8b0a
YouVin <dbqls990909@naver.com>
2025-04-21 16:45:37 +0900

    나머지 매개변수

--- workspace/ch01/ex01-43.js
+++ workspace/ch01/ex01-43.js
@@ -2,5 +2,16 @@
 나머지 매개변수(Rest parameters)
 */
 
+function fn(...args) {
+  console.log(args);
+  for (let arg of args) {
+    console.log(arg);
+  }
+}
 
-
+fn();
+fn(1);
+fn(1, 2);
+fn(1, 2, 3);
+fn(1, 2, 3, 4, 5);
+fn(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
