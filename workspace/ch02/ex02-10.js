0f37d52a2e609befedcb1325ddfdac8a51ebba29
YouVin <dbqls990909@naver.com>
2025-04-21 13:35:47 +0900

    함수 생성 4 (화살표 함수 표현식)

--- workspace/ch02/ex02-10.js
+++ workspace/ch02/ex02-10.js
@@ -3,22 +3,26 @@
 */
 
 // 기존 함수
-const add = function(x, y) {
+const add = function (x, y) {
   return x + y;
 };
 
 // 화살표 함수
+add = (x, y) => {
+  return x + y;
+};
 
-
-// 화살표 함수 축약
-
+// 화살표 함수 축약 중괄호 생략
+add = (x, y) => x + y;
 
 // 기존 함수
-const add10 = function(x) {
+const add10 = function (x) {
   return x + 10;
 };
 
 // 화살표 함수
-
-
+add10 = (x) => {
+  return x + 10;
+};
 // 화살표 함수 축약
+add10 = (x) => x + 10;
