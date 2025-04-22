d849aec25e8ae9048116618ef4bf849464114d24
YouVin <dbqls990909@naver.com>
2025-04-22 11:03:45 +0900

    this는 메서드를 정의한 객체

--- workspace/ch02/ex02-15.js
+++ workspace/ch02/ex02-15.js
@@ -3,3 +3,27 @@
   - this는 메서드를 정의한 객체
 */
 
+//window.name = "global"; // window, 브라우저가 가지고 있는 전역 객체
+//global.name = "global"; // global, Node.js 가 가지고 있는 전역 객체
+globalThis.name = "global"; // gobalThis, 모든 환경에서 가지고 있는 전역 객체
+
+const getPingName = function () {
+  return this.name;
+};
+
+const baro = new Object();
+baro.name = "바로핑";
+baro.age = 9;
+baro.getName = getPingName;
+
+console.log(getPingName());
+console.log(baro.age, baro.getName());
+
+const rara = {
+  name: "라라핑",
+  age: 8,
+  getName: getPingName,
+};
+
+console.log(getPingName());
+console.log(rara.age, rara.getName());
