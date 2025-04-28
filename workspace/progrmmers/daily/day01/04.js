600190240a15603b8d84c8188d32fc9b7d1718a6
YouVin <dbqls990909@naver.com>
2025-04-28 17:45:03 +0900

    1일차 프로그래머스

--- workspace/progrmmers/daily/day01/04.js
+++ workspace/progrmmers/daily/day01/04.js
@@ -18,16 +18,28 @@ aBcDeFg
 AbCdEfG
 */
 
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let input = [];
 
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
+rl.on("line", function (line) {
+  input = [line];
+}).on("close", function () {
+  str = input[0];
+
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toUpperCase() === str[i]) {
+      // 대문자면 소문자로
+      result = result + str[i].toLowerCase();
+    } else {
+      // 소문자면 대문자로
+      result += str[i].toUpperCase();
+    }
+  }
+  console.log(result);
 });
