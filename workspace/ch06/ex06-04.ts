6bb315d7134fbdc118db54edfd8929bae0464b4f
YouVin <dbqls990909@naver.com>
2025-04-28 11:32:18 +0900

    선택적 파라미터

--- workspace/ch06/ex06-04.ts
+++ workspace/ch06/ex06-04.ts
@@ -1,10 +1,11 @@
-// // 선택적 파라미터(optional parameter)
+// 선택적 파라미터(optional parameter)
 
-// (()=>{
-//   function user(name, age) {
-//     console.log(name, age);
-//   }
+(() => {
+  function user(name: string, age?: number): void {
+    //age는 선택적 파라미터로, undefined로 초기화 되어 출력된다.
+    console.log(name, age);
+  }
 
-//   user('하루', 5);
-
-// })();
+  user("하루", 5);
+  user("하루"); // age 는 undefined로 출력
+})();
