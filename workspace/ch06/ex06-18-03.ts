31cc29eee1fb15e5d9f2d80412e8985ecd99b3a8
YouVin <dbqls990909@naver.com>
2025-04-29 14:27:33 +0900

    드롭다운 리스트 생성 - 타입스크립트

--- workspace/ch06/ex06-18-03.ts
+++ workspace/ch06/ex06-18-03.ts
@@ -1,6 +1,64 @@
 // 드롭다운 리스트 생성 - 타입스크립트
 // ex06-18-02.js 복사
 
-(()=>{
-  
+// 1. interface 생성
+// 2. 객체에 배열 타입 지정
+
+(() => {
+  interface City {
+    value: string;
+    // 없어도 되니까 옵셔널 체이닝
+    selected?: boolean;
+  }
+
+  interface Zipcode {
+    value: number;
+    selected?: boolean;
+  }
+
+  const cityList: City[] = [
+    { value: "Seoul", selected: false },
+    { value: "busan" },
+    { value: "GwangJu", selected: true },
+    //{ value: 56789 },
+  ];
+
+  const zipcodeList: Zipcode[] = [
+    { value: 12345, selected: false },
+    { value: 34567, selected: true },
+    { value: 56789 },
+  ];
+
+  // FIXME: 아래 출력 결과와 같이 출력 되도록 함수 작성
+  function createDropdownList(list: City[] | Zipcode[]) {
+    let value;
+    let options = "";
+
+    for (let item of list) {
+      value = item.value;
+      options += `\t<option selected="${item.selected ? "selected" : ""}">${
+        typeof value === "string" ? value.toUpperCase() : value
+      }</option>\n`;
+    }
+
+    return `<select>\n${options}</select>`;
+  }
+
+  /* 출력 결과
+  <select>
+  <option selected="">seoul</option>
+  <option selected="">busan</option>
+  <option selected="selected">gwangju</option>
+  </select>
+  */
+  console.log(createDropdownList(cityList));
+
+  /*
+  <select>
+  <option selected="">12345</option>
+  <option selected="selected">34567</option>
+  <option selected="">56789</option>
+  </select>
+  */
+  console.log(createDropdownList(zipcodeList));
 })();
