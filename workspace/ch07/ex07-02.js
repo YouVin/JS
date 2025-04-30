acbba59ef89db9528f4a01b5af6ab15e26a6da79
YouVin <dbqls990909@naver.com>
2025-04-30 10:31:59 +0900

    7장 내장 함수, 내장 객체

---
+++ workspace/ch07/ex07-02.js
@@ -0,0 +1,11 @@
+"use strict";
+// 주요 내장 함수 - setTimeout
+(() => {
+    console.log("시작");
+    const timerId = setTimeout(() => {
+        console.log("3초 후에 실행 됩니다.");
+    }, 1000 * 3);
+    console.log("종료");
+    // timeout 작업 취소
+    clearTimeout(timerId);
+})();
