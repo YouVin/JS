2f43ec9f124cd192462dd3727e897b0fcdf5bc78
YouVin <dbqls990909@naver.com>
2025-04-30 17:16:42 +0900

    // 주요 내장 함수 - clearTimeout

--- workspace/ch07/ex07-03.js
+++ workspace/ch07/ex07-03.js
@@ -2,4 +2,11 @@
 // 주요 내장 함수 - clearTimeout
 // ex07-02.ts 복사
 (() => {
+    console.log("시작");
+    const timerId = setInterval((a, b) => {
+        console.log("3초 후에 실행 됩니다.");
+    }, 1000 * 3, 10, 20);
+    console.log("종료");
+    // timeout 작업 취소
+    //clearInterval(timerId);
 })();
