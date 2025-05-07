7bfa5697cc209d61afd4744e4ae7f235e6b9369e
YouVin <dbqls990909@naver.com>
2025-05-07 18:02:15 +0900

    비동기 함수의 작업이 완료된 후 실행할 코드가 있으면 async/await 사용

--- workspace/ch08/ex08-09-02.ts
+++ workspace/ch08/ex08-09-02.ts
@@ -2,6 +2,47 @@
 // 비동기 함수의 작업이 완료된 후 실행할 코드가 있으면 async/await 사용
 // ex08-09.ts 복사
 
-(()=>{
-  
+(() => {
+  function p1() {
+    // Promise
+    return new Promise((resolve, reject) => {
+      resolve("p1 결과");
+    });
+  }
+  async function a1() {
+    return "a1 결과";
+  }
+
+  function p2() {
+    return new Promise((resovle, reject) => {
+      reject("p2 에러");
+    });
+  }
+  async function a2() {
+    throw "a2 에러";
+  }
+
+  async function test() {
+    try {
+      const a1Result = await a1();
+      console.log(a1Result);
+
+      const p1Result = await p1();
+      console.log(p1Result);
+
+      const a2Result = await a2();
+      console.log(a2Result);
+
+      const p2Result = await p2();
+      console.log(p2Result);
+    } catch (err) {
+      console.error("실패", err);
+    }
+  }
+
+  console.log("1. 작업 시작.");
+  test();
+  console.log(
+    "2. 작업 종료. 이 메세지가 f1 작업 완료 보다 먼저 나오면 비동기로 호출된다는 의미"
+  );
 })();
