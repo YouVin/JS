2ff716652cbe165c61e78c65895971e9d4d31a40
YouVin <dbqls990909@naver.com>
2025-05-07 14:35:40 +0900

    merge

--- workspace/ch08/ex08-07.ts
+++ workspace/ch08/ex08-07.ts
@@ -4,22 +4,27 @@
 
 (() => {
   function f1() {
+    // 비동기 함수
     console.log("\t\t3. f1 호출됨.");
     const delay = Math.floor(Math.random() * 1000 * 10);
-    console.log(`\t\t ${delay}동안 작업중...`);
+    console.log(`\t\t4. ${delay}ms 동안 작업중...`);
 
-    set;
-    console.log("f1 리턴됨.");
+    setTimeout(() => {
+      console.log("\t\t f1 작업 완료.", delay);
+    }, delay);
+
+    console.log("\t\t5. f1 리턴됨.");
   }
 
   function test() {
-    console.log("test 호출됨.");
+    // 비동기
+    console.log("\t2. test 호출됨.");
     f1();
-    console.log("test 리턴됨");
+    f1();
+    console.log("\t6. test 리턴됨");
   }
 
-  // FIXME 프로그램 실행 흐름에 맞춰서 콘솔 출력 메세지 앞에 번호 추가
-  console.log("작업 시작.");
+  console.log("1. 작업 시작.");
   test();
-  console.log("작업 종료.");
+  console.log("7. 작업 종료.");
 })();
